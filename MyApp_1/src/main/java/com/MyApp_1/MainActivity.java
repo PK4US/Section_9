3431449f35dcfdf26b14d8115467b0ab32933569
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-10-08 14:36:41 +0300

    Урок 1. При каждом вызове метода onRestart() пишет обновлений +1

--- MyApp_1/src/main/java/com/MyApp_1/MainActivity.java
+++ MyApp_1/src/main/java/com/MyApp_1/MainActivity.java
@@ -1,8 +1,8 @@
 package com.MyApp_1;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +10,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        TextView tv2 = findViewById(R.id.tv2);
+        int num = Integer.parseInt(tv2.getText().toString());
+        num++;
+        tv2.setText(String.valueOf(num));
     }
 }
