a12b55f53528664f50215f01cd91c0879d7ca843
Stepanchuk Eugene <Stepanchuk.ei@gmail.com>
2020-10-09 21:47:32 +0300

    Урок 2. Используя Intent-фильтр, при клике по метке выведите второй Activity.

--- MyApp_2/src/main/java/com/MyApp_2/MainActivity.java
+++ MyApp_2/src/main/java/com/MyApp_2/MainActivity.java
@@ -1,14 +1,23 @@
 package com.MyApp_2;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
+    private static final String PK4U_SECOND_ACTIVITY = "com.PK4US.MY_SECOND_ACTIVITY";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+       findViewById(R.id.tv).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        Intent intent = new Intent(PK4U_SECOND_ACTIVITY);
+        startActivity(intent);
     }
 }
